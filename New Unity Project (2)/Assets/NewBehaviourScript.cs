297c85c797ac6b3648a6487fbe654c985f8b403d
gassen013 <40147681+gassen013@users.noreply.github.com>
2018-06-10 19:21:43 +0200

    nn

--- New Unity Project (2)/Assets/NewBehaviourScript.cs
+++ New Unity Project (2)/Assets/NewBehaviourScript.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        int c = 0;
         Debug.Log("hi");
-	}
+        int[] how = new int[11];
+        foreach (int i  in how)
+        {
+           
+            how[c] = 10;
+            c++;
+        }
+        foreach (int k in how)
+        {
+            Debug.Log(k);
+            Debug.Log("how[k]" + how[k]);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
